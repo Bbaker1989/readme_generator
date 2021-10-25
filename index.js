877646181ac55aef3190b6a8b8a150057c864513
bbaker1989 <brittneybaker@Brittneys-Air.lan>
2021-10-24 21:59:34 -0400

    pseudo code to outline the functionality of the readme generator and npm packages installed'

---
+++ index.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateReadme = require("./utils/generateReadme");
+
+//README Prompt Questions
+
+//Prompt for title of application  for  README
+
+// Description
+
+// Instillation Instructions
+
+//Usage
+
+//List of License for Application
+
+//Github Username
+
+//User Name Address
+
+//Questions
+
+//Generate Table of Contents
+
